bcbe53e4d937ff6cc0dc9dc8c8c87449d8b86ea4
whatrye <>
2020-06-15 09:34:13 +0800

    little change

--- getFileLinks4.py
+++ getFileLinks4.py
@@ -35,18 +35,30 @@ def getTandI_thread(tp,torrentsPath,enable_proxy = False, proxy_string = {"http"
     print(tp['link'])
     tResDict = gettorrentlink4.get_torrentlink(myreq_url = str(tp['link']), enable_proxy = enable_proxy, proxy_string = proxy_string)
     n = 0
+'''
+    #文件保存到各自目录下
     for imgLink in tResDict['imgsList']:
         #outfilename = imgLink[imgLink.rfind('/')+1:len(imgLink)]
+##        outfilename = imgLink[imgLink.rfind('/')+1:] #获取原文件名
+        #图片文件名替换为title+序号格式，如must1.jpg must2.jpg ...，和torrent文件统一文件名
+        n = n+1
+        outfilename = tResDict['title']+ '_' + str(n) + imgLink[imgLink.rfind('.'):]
+        a = {'link':imgLink,'ofile':outfilename,'oDir':str(torrentsPath + r'/' +tResDict['title'])}
+        imgsList.append(a)
+    if tResDict['btCode'] != 'notExist':
+        b = {'link':tResDict['btCode'],'ofile':str(tResDict['title'])+'.torrent','oDir':str(torrentsPath + r'/' +tResDict['title'])}
+        btsList.append(b)
+'''        
+    #保存所有文件到torrents目录下
+    for imgLink in tResDict['imgsList']:
 ##        outfilename = imgLink[imgLink.rfind('/')+1:]
         #图片文件名替换为title+序号格式，如must1.jpg must2.jpg ...，和torrent文件统一文件名
         n = n+1
         outfilename = tResDict['title']+ '_' + str(n) + imgLink[imgLink.rfind('.'):]
-##        a = {'link':imgLink,'ofile':outfilename,'oDir':str(torrentsPath + r'/' +tResDict['title'])}
         #change path to 'torrents'
         a = {'link':imgLink,'ofile':outfilename,'oDir':torrentsPath}
         imgsList.append(a)
     if tResDict['btCode'] != 'notExist':
-##        b = {'link':tResDict['btCode'],'ofile':str(tResDict['title'])+'.torrent','oDir':str(torrentsPath + r'/' +tResDict['title'])}
         b = {'link':tResDict['btCode'],'ofile':str(tResDict['title'])+'.torrent','oDir':torrentsPath}
         btsList.append(b)
         
